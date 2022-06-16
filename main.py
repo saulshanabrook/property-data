3cef92c6ad29f84dd489e8d8f7b6a2365b2f2b70
Saul Shanabrook <s.shanabrook@gmail.com>
2022-06-16 16:13:30 -0400

    Update data

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import functools
 import subprocess
 import zipfile
 from io import BytesIO
@@ -5,6 +6,7 @@ from pathlib import Path
 
 import pandas as pd
 import requests
+from bs4 import BeautifulSoup
 from dbfread import DBF
 from rich.progress import track
 from sqlite_utils import Database
@@ -20,12 +22,50 @@ TOWNS = [
     "Leyden",
     "Colrain",
     "Wendell",
+    "Heath",
+    "Northfield",
+    "Buckland",
+    "Shelburne",
+    "Ashfield",
+    "Cummington",
+    "Goshen",
+    "Whately",
+    "Sunderland",
+    "Leverett",
+    "Shutesbury",
+    "New Salem",
 ]
 
 DB_FILE = "property-data.db"
 
 
-def get_town_download_urls() -> list[tuple[str, str]]:
+def get_town_values() -> dict[str, str]:
+    page = requests.get(
+        "https://dlsgateway.dor.state.ma.us/gateway/DLSPublic/ParcelSearch"
+    )
+    soup = BeautifulSoup(page.text, "html.parser")
+    return {
+        o.text: o["value"]
+        for o in soup.find(id="SelectedJurisdictions").find_all("option")
+    }
+
+
+def get_sales():
+    print("Getting sales")
+    p = Path("data/LA3ParcelSearch.xlsx")
+    if p.exists():
+        return
+    values = get_town_values()
+    data = [("SelectedJurisdictions", values[town]) for town in TOWNS]
+    response = requests.post(
+        "https://dlsgateway.dor.state.ma.us/gateway/DLSPublic/ParcelSearch/ExportToExcel",
+        data=data,
+    )
+    p.write_bytes(response.content)
+
+
+@functools.cache
+def get_town_download_urls_by_town() -> dict[str, str]:
     # https://www.mass.gov/info-details/massgis-data-property-tax-parcels#downloads-
     download_urls_df = pd.read_excel(
         "https://www.mass.gov/doc/massgis-parcel-data-download-links-table/download"
@@ -34,41 +74,49 @@ def get_town_download_urls() -> list[tuple[str, str]]:
         r["Town Name"]: r["Shapefile Download URL"]
         for r in download_urls_df.to_dict("records")
     }
-    return [download_urls_by_town[name.upper()] for name in TOWNS]
+    return download_urls_by_town
 
 
 def main():
-    if not list(Path("data").glob("*/*.shp")):
-        for url in track(get_town_download_urls(), "Downloading town data..."):
-            r = requests.get(url)
-            z = zipfile.ZipFile(BytesIO(r.content))
-            z.extractall("data")
-    db = Database(DB_FILE)
-    added_use_codes = False
-    for town_dir in track(
-        [p for p in Path("data").iterdir() if p.is_dir()], "Converting to sqlite..."
-    ):
-        for table in ["OthLeg", "TaxPar"]:
-            (shp_file,) = town_dir.glob(f"*{table}*.shp")
-            subprocess.run(
-                [
-                    "shapefile-to-sqlite",
-                    DB_FILE,
-                    "--table",
-                    table,
-                    str(shp_file),
-                    "--spatialite_mod=mod_spatialite.dylib",
-                    "--prefix-pk",
-                ],
-                check=True,
-            )
-        for table in ["Assess", "UC_LUT"]:
-            if table == "UC_LUT":
-                if added_use_codes:
-                    continue
-                added_use_codes = True
-            (dbf_file,) = town_dir.glob(f"*{table}*.dbf")
-            db[table].insert_all(DBF(dbf_file))
+    get_sales()
+    # not_downloaded_towns = [t for t in TOWNS if not list(Path("data").glob(f"*{t}"))]
+    # for town in track(not_downloaded_towns, "Downloading town data..."):
+    #     url = get_town_download_urls_by_town()[town.upper()]
+    #     r = requests.get(url)
+    #     z = zipfile.ZipFile(BytesIO(r.content))
+    #     z.extractall("data")
+    db = Database(DB_FILE, recreate=False)
+    # added_use_codes = False
+    # for town_dir in track(
+    #     [p for p in Path("data").iterdir() if p.is_dir()], "Converting to sqlite..."
+    # ):
+    #     for table in ["OthLeg", "TaxPar"]:
+    #         (shp_file,) = town_dir.glob(f"*{table}*.shp")
+    #         subprocess.run(
+    #             [
+    #                 "shapefile-to-sqlite",
+    #                 DB_FILE,
+    #                 "--table",
+    #                 table,
+    #                 str(shp_file),
+    #                 "--spatialite_mod=mod_spatialite.dylib",
+    #                 "--prefix-pk",
+    #             ],
+    #             check=True,
+    #         )
+    #     for table in ["Assess", "UC_LUT"]:
+    #         if table == "UC_LUT":
+    #             if added_use_codes:
+    #                 continue
+    #             added_use_codes = True
+    #         (dbf_file,) = town_dir.glob(f"*{table}*.dbf")
+    #         db[table].insert_all(DBF(dbf_file))
+    # db.
+    db.create
+    df = pd.read_excel(
+        "data/LA3ParcelSearch.xlsx", skiprows=4, parse_dates=["Sale Date"]
+    ).to_sql("LA3", db.conn, if_exists="append", index=False)
+
     db.create_view(
         "parcels",
         """
@@ -122,59 +170,3 @@ from
 
 
 main()
-
-# """
-# with props as (
-#   select
-#     a.BLDG_VAL,
-#     a.LAND_VAL,
-#     a.OTHER_VAL,
-#     a.TOTAL_VAL,
-#     a.FY,
-#     a.LOT_SIZE,
-#     a.LS_DATE,
-#     a.LS_PRICE,
-#     a.SITE_ADDR,
-#     a.ADDR_NUM,
-#     a.FULL_STR,
-#     a.LOCATION,
-#     a.CITY,
-#     a.ZIP,
-#     a.OWNER1,
-#     a.OWN_ADDR,
-#     a.OWN_CITY,
-#     a.OWN_STATE,
-#     a.OWN_ZIP,
-#     a.OWN_CO,
-#     a.LS_BOOK,
-#     a.LS_PAGE,
-#     a.REG_ID,
-#     a.ZONING,
-#     a.YEAR_BUILT,
-#     a.BLD_AREA,
-#     a.UNITS,
-#     a.RES_AREA,
-#     a.STYLE,
-#     a.STORIES,
-#     a.NUM_ROOMS,
-#     a.LOT_UNITS,
-#     a.CAMA_ID,
-#     tp.geometry,
-#     tp.LOC_ID as rowid
-#   from
-#     Assess a
-#     inner join TaxPar tp using(LOC_ID)
-# ),
-# codes as (
-#   select
-#     a.LOC_ID as rowid,
-#     lut.USE_DESC
-#   FROM
-#     Assess a
-#     left join UC_LUT lut using(USE_CODE)
-# )
-# select
-#   *
-# from
-#   codes
-# """
